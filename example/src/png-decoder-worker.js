104c79ff3421cbd1dde7774abddb5c8b1a134d85
Nikolay Garmash <mykola.harmash@here.com>
2019-01-11 11:59:01 +0100

    Updates

--- example/src/png-decoder-worker.js
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright © 2017-2018 HERE Europe B.V.
- * Licensed under Apache 2.0, see full license in LICENSE
- * SPDX-License-Identifier: Apache-2.0
- */
-
-require('@here/harp-terrain-datasource/src/png/tile-decoder-worker')
